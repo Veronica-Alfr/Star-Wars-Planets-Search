e8530fc6776338195e084b182792105e73768726
Veronica-Alfr <veagalves@gmail.com>
2022-04-18 19:26:48 -0300

    Tabela com 13 colunas criada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Provider from './AppContext/Provider';
+import Table from './Componets/Table';
 
 function App() {
   return (
     <div>
       <Provider>
-        <span>Hello, App!</span>
+        <h1>Star Wars</h1>
+        <Table />
       </Provider>
     </div>
   );
--- src/AppContext/Provider.js
+++ src/AppContext/Provider.js
@@ -1,19 +1,27 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import Context from './Context';
 
 const Provider = ({ children }) => {
-  const [data, setData] = useState([]);
+  const [planets, setPlanets] = useState({
+    data: [],
+  });
   useEffect(() => {
-    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, [data]);
+    const getData = async () => {
+      const dataPlanets = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const response = await dataPlanets.json();
+      console.log(dataPlanets);
+      setPlanets((prevData) => ({ ...prevData, data: response.results }));
+    };
+    getData();
+  }, []);
   return (
-    <Context.Provider>
+    <Context.Provider value={ { planets, setPlanets } }>
       {children}
     </Context.Provider>
   );
+  // Ajuda de Halister Santos na requisição da API.
+  // Ajuda de Leonardo Araújo na exportação do Provider.
 };
 
 Provider.propTypes = {

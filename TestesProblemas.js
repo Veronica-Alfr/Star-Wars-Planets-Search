fa159cd383643986d96861b00df6e546253a106f
Veronica-Alfr <veagalves@gmail.com>
2022-04-23 15:53:56 -0300

    req 4 in progress

--- TestesProblemas.js
+++ TestesProblemas.js
@@ -1,7 +1,8 @@
-const array = [{ diam: 10, popul: 1000 }, { diam: 100, popul: 2000 }];
+const array = [{ diam: 10, popul: 1000 }, { diam: 100, popul: 2000 },
+  { diam: 30, popul: 5000 }];
 const array2 = [{ column: 'diam', comparison: 'maior que', value: 50 },
   { column: 'popul', comparison: 'maior que', value: 1500 },
-  { column: 'popul', comparison: 'menor que', value: 1000 }];
+  { column: 'popul', comparison: 'igual a', value: 2000 }];
 const test = array2.map((el) => array.filter((e) => {
   if (el.comparison === 'maior que') {
     return e[el.column] > el.value;
@@ -14,4 +15,4 @@ const test = array2.map((el) => array.filter((e) => {
   }
   return e;
 }));
-console.log(test[test.length - 1]); // Ajuda de Leo Araújo na lógica do requisito 3 e 4.
+console.log(test); // Ajuda de Leo Araújo na lógica do requisito 3 e 4.
